569e84f07cc2b23ced103f13cc43f2e38c8ae122
Carol <piggycdz192@yahoo.com>
2014-02-24 16:40:48 +0800

    nav bar

--- application/views/safetrip/homepageform.php
+++ application/views/safetrip/homepageform.php
@@ -20,28 +20,8 @@
     <![endif]-->
   </head>
   <body>
-    <div class="navbar navbar-inverse navbar-maincolor navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/safetrip">Safe Trip</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav pull-right pull-left-mobile">
-            <li><a href ="index.php/statistics">Statistics</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact Us</a></li>
-            <li><a href="#contact">FAQs</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-    
+    <?php include 'header.php'; ?>
+
     <div class="search-page">
       <div class="container">
         <div class="jumbotron transparent narrow-container">
@@ -80,6 +60,7 @@
           </div>
         </div>
         <div class="narrow-container">
+          <br>
         <?php
           $data = array(
             'class' => "btn btn-primary btn-lg pull-right",
@@ -91,7 +72,7 @@
 
           echo form_close();
 
-        ?>
+        ?> </br>
       </div>
       </div>
     </div>
